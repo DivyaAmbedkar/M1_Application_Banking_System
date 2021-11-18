905e2fa905e6a93f9226c3fa0916f738b8f2e589
Kavin Vignes J K <kavin02062000@gmail.com>
2021-11-18 15:06:43 +0530

    Update onlinebanking.c

--- 3_Implementation/onlinebanking.c
+++ 3_Implementation/onlinebanking.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
+/**
+ * @file onlinebanking.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-18
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "onlinebanking.h"
 {
  
 }
