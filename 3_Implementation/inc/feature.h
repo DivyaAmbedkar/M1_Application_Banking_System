0d989c201d9850eb321d5d36b3d3322a6468246d
Kavin Vignes J K <kavin02062000@gmail.com>
2021-11-23 17:29:29 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -12,14 +12,10 @@
 #ifndef FEATURE_H_
 #define FEATURE_H_
 
-#include "stdio.h"
-#include "stdlib.h"
-
 /**
  * @brief (Declaring all the functions)
  * 
  */
-
 void checkbalance(char*);
 void transfermoney(void);
 void display(char*);
@@ -30,5 +26,37 @@ void account(void);
 void accountcreated(void);
 void afterlogin(void);
 void logout(void);
+int getch(void);
+
+/**
+ * @brief (Creating a structure to store data of the user)
+ * 
+ */
+struct pass {
+	char username[50];
+	int date, month, year;
+	char pnumber[15];
+	char adharnum[20];
+	char fname[50];
+	char lname[50];
+	char fathname[50];
+	char mothname[50];
+	char address[100];
+	char typeaccount[50];
+};
+
+/**
+ * @brief (Structure to keep track of amount transfer)
+ * 
+ */
+struct money {
+	char usernameto[50];
+	char userpersonfrom[50];
+	long int money1;
+};
+
+struct userpass {
+	char password[50];
+};
 
 #endif
